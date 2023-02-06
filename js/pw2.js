b70333876de5dd55ae25a5ffdea5024452b84d87
marietaoko <123906703+marietaoko@users.noreply.github.com>
2023-02-06 13:27:40 -0500

    Add files via upload

--- js/pw2.js
+++ js/pw2.js
@@ -1,10 +1,13 @@
 
   const open = document.querySelector('.b1');
   const open2 = document.querySelector('.b2');
+  const open3 = document.querySelector('.b3');
   const close = document.querySelector('#closeMenu');
   const close2 = document.querySelector('#closeMenu2');
+  const close3 = document.querySelector('#closeMenu3');
   const nav = document.querySelector('.overlay');
   const nav2 = document.querySelector('.overlay2');
+  const nav3 = document.querySelector('.overlay3');
   
   
   open.addEventListener('click', function(){
@@ -14,6 +17,10 @@
   open2.addEventListener('click', function(){
     nav2.style.height = '50vh';
   });
+
+  open3.addEventListener('click', function(){
+    nav3.style.height = '50vh';
+  });
   
   close.addEventListener('click', function(){
     nav.style.height = '0';
@@ -22,5 +29,11 @@
   close2.addEventListener('click', function(){
     nav2.style.height = '0';
   });
+
+  close3.addEventListener('click', function(){
+    nav3.style.height = '0';
+  });
+
+  
 
 
