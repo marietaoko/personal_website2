993b7ca063f66df74749a99828c07916f0a65a9d
marietaoko <123906703+marietaoko@users.noreply.github.com>
2023-03-10 09:13:26 -0500

    Add files via upload

--- js/pw2.js
+++ js/pw2.js
@@ -3,14 +3,17 @@
   const open2 = document.querySelector('.b2');
   const open3 = document.querySelector('.b3');
   const open4 = document.querySelector('.b4');
+  const open5 = document.querySelector('.b5');
   const close = document.querySelector('#closeMenu');
   const close2 = document.querySelector('#closeMenu2');
   const close3 = document.querySelector('#closeMenu3');
   const close4 = document.querySelector('#closeMenu4');
+  const close5 = document.querySelector('#closeMenu5');
   const nav = document.querySelector('.overlay');
   const nav2 = document.querySelector('.overlay2');
   const nav3 = document.querySelector('.overlay3');
   const nav4 = document.querySelector('.overlay4');
+  const nav5 = document.querySelector('.overlay5');
   
   
   open.addEventListener('click', function(){
@@ -28,6 +31,10 @@
   open4.addEventListener('click', function(){
     nav4.style.height = '50vh';
   });
+
+  open5.addEventListener('click', function(){
+    nav5.style.height = '50vh';
+  });
   
   close.addEventListener('click', function(){
     nav.style.height = '0';
@@ -43,6 +50,10 @@
 
   close4.addEventListener('click', function(){
     nav4.style.height = '0';
+  });
+
+  close5.addEventListener('click', function(){
+    nav5.style.height = '0';
   });
 
   
